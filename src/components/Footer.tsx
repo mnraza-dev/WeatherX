a4e2374faffea72d06667177464fdc8c7f36db48
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-29 17:06:46 +0530

    fix: enhance footer styling for improved visibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer: React.FC = () => {
     return (
-        <footer className='border-t backdrop-blur-sm py-12 '>
+        <footer className='border-t backdrop-blur-sm py-12 supports-[backdrop-filter]:bg-background/60 '>
             <div className='container mx-auto px-4 text-center text-gray-400'>
                 <p className='text-lg '>Made with ♥️ by <Link target='_blank' to={"https://www.x.com/mnraza1907"}>MN Raza</Link> </p>
             </div>
