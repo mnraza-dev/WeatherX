7fbea568844d762b6253079ae162ff8d3603ae64
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-29 17:09:23 +0530

    feat: implement routing for Weather Dashboard and add main layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import { ThemeProvider } from "@/context/theme-provider"
+import WeatherDashboard from './components/WeatherDashboard'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Layout >
-          <h1>Hello there</h1>
+       
+       <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/weather-dashboard" element={<WeatherDashboard/>} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1>Home</h1>} />
+       </Routes>
         </Layout>
       </ThemeProvider>
     </BrowserRouter>
---
+++ src/components/WeatherDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeatherDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Weather Dashboard</h1>
+        </div>
+    )
+}
+
+export default WeatherDashboard
