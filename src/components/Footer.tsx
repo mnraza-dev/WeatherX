ef9c3086f061e3b3404a1f9d482e1562f547d4b4
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-29 16:56:05 +0530

    fix: add 'by' to the footer attribution for clarity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer: React.FC = () => {
     return (
         <footer className='p-8 bg-blue-100  flex items-center justify-center'>
-            <p className='text-lg '>Made with ♥️ <Link className='text-blue-800 leading-0.5' to={"https://www.x.com/mnraza1907"}>MN Raza</Link> </p>
+            <p className='text-lg '>Made with ♥️ by <Link className='text-blue-800 leading-0.5' target='_blank' to={"https://www.x.com/mnraza1907"}>MN Raza</Link> </p>
         </footer>
     )
 }
